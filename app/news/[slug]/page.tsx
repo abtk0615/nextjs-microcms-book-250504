7001b2119aa02dffa561a04f9dd59cc8c2f0656d
Takahiro Abe <taka8592@gmail.com>
2025-06-03 21:32:25 +0900

    第7章まで完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,18 +1,24 @@
-import { notFound } from "next/navigation";
-import { getNewsDetail } from "@/app/_libs/microcms";
-import Article from "@/app/_components/Article";
-import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
+import { notFound } from 'next/navigation';
+import { getNewsDetail } from '@/app/_libs/microcms';
+import Article from '@/app/_components/Article';
+import ButtonLink from '@/app/_components/ButtonLink';
+import styles from './page.module.css';
 
 type Props = {
   params: {
     slug: string;
   };
+  searchParams: {
+    dk?: string;
+  };
 };
 
-export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(notFound);
-  return(
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  }).catch(notFound);
+
+  return (
     <>
       <Article data={data} />
       <div className={styles.footer}>
